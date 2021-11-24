9ae48331d60fcc9cbd9d87a21644b73004932853
zhaolx <zhaolixin2001@163.com>
2021-11-24 19:27:12 +0800

    idea第一次提交

---
+++ src/main/java/com/atguigui/GItTest.java
@@ -0,0 +1,11 @@
+package com.atguigui;
+
+/**
+ * @author zhao
+ * @create 2021-11-24 19:24
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git !!!!!!");
+    }
+}
