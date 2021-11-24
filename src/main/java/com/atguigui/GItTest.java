a7b7f731c5e85a38738d097bfca459360af83654
zhaolx <zhaolixin2001@163.com>
2021-11-24 19:40:23 +0800

    第二次提交

--- src/main/java/com/atguigui/GItTest.java
+++ src/main/java/com/atguigui/GItTest.java
@@ -7,5 +7,6 @@ package com.atguigui;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello Git !!!!!!");
+        System.out.println("hello Git 22  !!!!!!");
     }
 }
